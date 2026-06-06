f1e33acafff91d9620a2d4ec27a1d40a068f60f8
Andika Setiadi <andikasetiadi132@gmail.com>
2026-06-06 18:48:12 +0700

    last

--- script.js
+++ script.js
@@ -325,3 +325,14 @@ Infinity
 );
 
 });
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+document.querySelectorAll("#nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
